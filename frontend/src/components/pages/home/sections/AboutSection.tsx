b8471eea48bd4732f5081034832bfa0e1e7572a2
ayssag <ayssanaara@gmail.com>
2026-09-21 15:14:19 -0300

    feat: Creates SectionAboutUs

--- frontend/src/components/pages/home/sections/AboutSection.tsx
+++ frontend/src/components/pages/home/sections/AboutSection.tsx
@@ -1,13 +1,37 @@
-import { Typography } from "@mui/material"
+import { Typography, Stack, Box } from "@mui/material"
 import { BoxSection, FlexBoxBetween } from "@/shared/styled"
-import { t } from "i18next"
+import { useTranslation } from "react-i18next"
+import unballLogo from "@/assets/images/logos/unball-logo-vector.svg"
 
 function AboutSection() {
+    const { t } = useTranslation();
     return (
         <BoxSection>
-            <FlexBoxBetween>
-                <Typography variant="h2" color="text.primary">{t("pages.home.about.title")}</Typography>
-                <Typography>{t("pages.home.about.description")}</Typography>
+            <FlexBoxBetween
+                sx={{
+                    width: { xs: "100%", lg: "60%"},
+                    flexDirection: { xs: "column", md: "row" },
+                    gap: { xs: 4, md: 6 },
+                    alignItems: "center"
+                }}>
+                <Box
+                    component="img"
+                    src={unballLogo}
+                    alt="UnBall Logo"
+                    sx={{
+                        width: { xs: "180px", sm: "240px", md: "280px" },
+                        height: "auto",
+                        maxWidth: "100%",
+                        objectFit: "contain",
+                    }}
+                />
+                <Stack
+                    spacing={2}
+                    sx={{ maxWidth: { md: "50%" } }}>
+                    <Typography variant="h6">{t("pages.home.about.subtitle")}</Typography>
+                    <Typography variant="h2" color="text.primary">{t("pages.home.about.title")}</Typography>
+                    <Typography>{t("pages.home.about.description")}</Typography>
+                </Stack>
             </FlexBoxBetween>
         </BoxSection>
     )
--- frontend/src/i18n/locales/pt/pages/home/about.ts
+++ frontend/src/i18n/locales/pt/pages/home/about.ts
@@ -1,5 +1,5 @@
 export const about = {
-    title: "QUEM SOMOS",
+    title: "SOBRE NÓS",
     subtitle: "// QUEM É A UNBALL",
     description: "Equipe de futebol de robôs e projeto de extensão formado por estudantes da Universidade de Brasília. Projetamos e construímos robôs 100% autônomos para as ligas VSSS e SSL. Conheça a nossa trajetória e conquistas. "
 };
